cbe3ae0f70de8adc3cf672c79ed06f0e5f513d71
romeychristo <romeychristo.d@gmail.com>
2025-06-23 20:30:01 -0400

    add readme file add comments to problem 26

--- 26. Remove Duplicates from Sorted Array/index.js
+++ 26. Remove Duplicates from Sorted Array/index.js
@@ -1,18 +1,22 @@
 function removeDuplicates(nums) {
+    // If the array is empty, return 0
     if (nums.length === 0) {
         return 0;
     }
     else {
-        var i = 1;
+        var i = 1; // Pointer for the next unique element
+        // Start from the second element and iterate through the array
         for (var j = 1; j < nums.length; j++) {
+            // If current element is not equal to the previous one, it's unique
             if (nums[j] !== nums[j - 1]) {
-                nums[i] = nums[j];
-                i += 1;
+                nums[i] = nums[j]; // Place the unique element at index i
+                i += 1; // Move to the next position for unique elements
             }
         }
+        // Return the number of unique elements
         return i;
     }
 }
 var nums = [1, 1, 1, 2, 3, 3, 4, 5, 5, 5, 6];
 var answer = removeDuplicates(nums);
-console.log(answer);
+console.log(answer); // Output the count of unique elements
